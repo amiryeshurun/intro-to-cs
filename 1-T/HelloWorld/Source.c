ba9cc04ee51cff834511b825645185d831871140
amiryeshurun <amiryeshooroon@gmail.com>
2020-11-05 15:09:28 +0200

    first lesson

---
+++ 1-T/HelloWorld/Source.c
@@ -0,0 +1,17 @@
+/*
+	Name: Amir Yeshurun
+	Date: 05/11
+	Id: 212125439
+*/
+
+// standard input output
+#include <stdio.h>
+
+// # - preprocessor / precompiler
+
+void main()
+{   // block
+	// This is a test
+	// \n - new line
+	printf("Hello World My name\n is amir\n\n\n\n");
+}
